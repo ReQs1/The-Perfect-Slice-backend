1be93ef74d80cd8c50362abbc54ce1719eea68fd
Req <snogedirl@gmail.com>
2025-03-11 17:47:00 +0100

    increase maximum authentication attempts limit to 50

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const globalLimiter = rateLimit({
 
 const authLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 10,
+  max: 50,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Too many authentication attempts, please try again later",
